2ffe81bdb2e23a3d231317ede86e38ba64440e88
Shao-YJ <914691325@qq.com>
2022-09-20 15:25:40 +0800

    移动了一些文件

---
+++ List/List.h/mlist.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"std.h"
+
+typedef struct{
+	int* elem;
+	int n;
+	int N;
+}Mlist;
+
+
+void initMlist(Mlist &L, int length);
+
+void MlisttoString(Mlist L);
+
+void insertMlist(Mlist &L, int e);
+void insertMlist(Mlist& L, int i, int e);
+
+void deleteMlist(Mlist& L, int i);
+
+int locateMlist(Mlist& L, int e);
+
+void mergeMlist(Mlist L, Mlist R, Mlist& LR);
+
+bool MlistisEmpty(Mlist L);
+
+void freeMlist(Mlist& L);
