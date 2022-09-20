06696b55dac5ab0f97001938c15500f3996686d9
Shao-YJ <914691325@qq.com>
2022-09-20 12:02:10 +0800

    stack 和queue基本操作

--- 源.cpp
+++ 源.cpp
@@ -1,48 +1,54 @@
-#include<iostream>
-#include<stdlib.h>
-#include"MList.h"
-#include"LinkList.h"
-using namespace std;
+#include"std.h"
+
+void stackTest();
+void queueTest();
 
 int main() {
 
-	/*LinkList node;
-	cirinitList(node);
-
-	insertList(node, 100);
-	insertList(node, 131);
-	ListToString(node);
-	int a[5] = { 1,92,125,154,165 };
-	LinkList node1;
-	cirinitList(node1);
-	insertNList(node1,a,5);
-	ListToString(node1);
-
-	mergeList(node, node1);
-	connectList(node, node1);
-	ListToString(node);
-
-	circularMerge(node, node1);
-	ListToString(node);
-
-	josephus(node,7,2,2);*/
-
-	DLinkList dnode;
-	initDList(dnode);
-	insertDList(dnode, 12);
-	insertDList(dnode, 123);
-	insertDList(dnode, 1562);
-	insertDList(dnode, 152);
-	insertDList(dnode, 156);
-
-	insertDList(dnode, 3, 0);
-
-	ListToString(dnode);
-
-	deleteDList(dnode, 2);
-	ListToString(dnode);
+	queueTest();
+	
 
 	return 0;
+}
+
+void queueTest() {
+	Queue q1;
+
+	initQueue(q1, 10);
+	for (int i = 0; i < 10; i++) 
+		inQueue(q1, i);
+	queueToString(q1);
+	
+	inQueue(q1, 11);
+	queueToString(q1);
+
+	//int n = outQueue;
+	cout << outQueue(q1)<<endl;
+	queueToString(q1);
+}
+
+void stackTest() {
+	Stack s1;
+
+	initStack(s1,5);
+
+	for (int i = 0; i < 5; i++)
+		push(s1, i);
+	stackToString(s1);
+	int n=pop(s1);
+	cout << n << endl;
+	stackToString(s1);
+
+	LinkStack s2;
+	initLinkStack(s2);
+	for (int i = 0; i < 5; i++)
+		push(s2, i*2);
+	stackToString(s2);
+	n = pop(s2);
+	cout << n << endl;
+	stackToString(s2);
+
+
 }
 
 
